9760082d10929469059557d41078efe2f93d846d
Cristian Lupse <cristian.lupse@arobs.com>
2020-01-31 11:32:05 +0200

    initiat my branch

--- src/App.js
+++ src/App.js
@@ -3,29 +3,7 @@ import Navbar from "./components/navbar";
 import './header.css';
 
 import './App.css';
-<<<<<<< HEAD
-import OpenPopup from "./components/OpenPopup";
 
-function App() {
-  return (
-    <div className="App">
-      <OpenPopup/>
-      {/*<header className="App-header">*/}
-      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.js</code> and save to reload.*/}
-      {/*  </p>*/}
-      {/*  <a*/}
-      {/*    className="App-link"*/}
-      {/*    href="https://reactjs.org"*/}
-      {/*    target="_blank"*/}
-      {/*    rel="noopener noreferrer"*/}
-      {/*  >*/}
-      {/*    Learn React*/}
-      {/*  </a>*/}
-      {/*</header>*/}
-    </div>
-=======
 import RoutesComponent from './RoutesComponent';
 import {BrowserRouter as Router} from 'react-router-dom';
 import './styles/style.css';
@@ -39,7 +17,7 @@ function App() {
           <GameList />
       </Router>
 
->>>>>>> develop
+
   );
 }
 
